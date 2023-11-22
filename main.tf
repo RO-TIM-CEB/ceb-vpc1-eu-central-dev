16e3df24efcfe0455754a444dc344f3c379cabbc
Alexandru Raul <alexandru.raul@gmail.com>
2023-11-22 14:58:07 +0200

    Adding new backend

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+
+ required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
   backend "s3" {
     bucket = "state-terraform-s3-ceb-vpc-dev"
     key    = "dev/europe/central-1/ceb-vpc1-eu-central-dev"
